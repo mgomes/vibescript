0c23357e9139052342a865f31c7236ca6ba515a8
Mauricio Gomes <mauricio@edge14.com>
2025-12-26 19:54:35 -0500

    Tighten up env implementation

--- vibes/env.go
+++ vibes/env.go
@@ -1,12 +1,35 @@
 package vibes
 
+import "sync"
+
 type Env struct {
 	parent *Env
 	values map[string]Value
 }
 
+var envPool = sync.Pool{
+	New: func() any {
+		return &Env{values: make(map[string]Value)}
+	},
+}
+
 func newEnv(parent *Env) *Env {
 	return &Env{parent: parent, values: make(map[string]Value)}
+}
+
+func borrowEnv(parent *Env) *Env {
+	env := envPool.Get().(*Env)
+	env.parent = parent
+	// clear any previous values
+	for k := range env.values {
+		delete(env.values, k)
+	}
+	return env
+}
+
+func releaseEnv(env *Env) {
+	env.parent = nil
+	envPool.Put(env)
 }
 
 func (e *Env) Get(name string) (Value, bool) {
