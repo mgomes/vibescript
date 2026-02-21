6225cf79907538865a64efa198df26697eea0177
Mauricio Gomes <mauricio@edge14.com>
2026-02-20 23:23:24 -0500

    extract function invocation checks from script call

--- vibes/execution_script.go
+++ vibes/execution_script.go
@@ -58,24 +58,9 @@ func (s *Script) Call(ctx context.Context, name string, args []Value, opts CallO
 		return NewNil(), err
 	}
 
-	if err := exec.pushFrame(fn.Name, fn.Pos); err != nil {
-		return NewNil(), err
-	}
-	val, returned, err := exec.evalStatements(fn.Body, callEnv)
-	exec.popFrame()
+	val, err := executeFunctionForCall(exec, fn, callEnv)
 	if err != nil {
 		return NewNil(), err
-	}
-	if fn.ReturnTy != nil {
-		if err := checkValueType(val, fn.ReturnTy); err != nil {
-			return NewNil(), exec.errorAt(fn.Pos, "%s", formatReturnTypeMismatch(fn.Name, err))
-		}
-	}
-	if err := exec.checkMemoryWith(val); err != nil {
-		return NewNil(), err
-	}
-	if returned {
-		return val, nil
 	}
 	return val, nil
 }
