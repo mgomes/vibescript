2467a343f5be346a1d0727adefdde00d47af94ae
Mauricio Gomes <mauricio@edge14.com>
2026-06-27 16:23:23 -0400

    Route illumos module opens through nonblocking path

--- internal/runtime/modules_open_other.go
+++ internal/runtime/modules_open_other.go
@@ -1,4 +1,4 @@
-//go:build !aix && !darwin && !dragonfly && !freebsd && !linux && !netbsd && !openbsd && !solaris
+//go:build !aix && !darwin && !dragonfly && !freebsd && !illumos && !linux && !netbsd && !openbsd && !solaris
 
 package runtime
 
